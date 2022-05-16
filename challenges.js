0b92155f8b15659bb6be103876f5c504f9e8f381
NatalieHumphriesGitHub <99325972+NatalieHumphriesGitHub@users.noreply.github.com>
2022-05-16 11:58:47 +0100

    challenge 4

--- challenges.js
+++ challenges.js
@@ -46,3 +46,33 @@ const agreeOrDisagree = (string1, string2) => {
 // Uncomment the line below when you're ready to try out your function
 console.log(agreeOrDisagree("yep", "yep")) 
 // Should print 'You agree!'
+
+/* Challenge 4 
+Write a function, lifePhase(), that takes in a person’s age, as a number, and returns which phase of life they are in.
+
+Here are the classifications:
+0-3 should return 'baby'
+4-12 should return 'child'
+13-19 should return 'teen'
+20-64 should return 'adult'
+65-140 should return 'senior citizen'
+If the number is less than 0 or greater than 140, the program should return 'This is not a valid age'*/
+
+// Write your function here:
+const lifePhase = age => {
+  if (age < 0 || age > 140) {
+    return 'This is not a valid age';
+  } else if (age < 4) {
+    return 'baby';
+  } else if (age < 13) {
+    return 'child';
+  } else if (age < 20) {
+    return 'teen';
+  } else if (age < 65) {
+    return 'adult'
+  } else {
+    return 'senior citizen'
+  }
+};
+
+console.log(lifePhase(99))
